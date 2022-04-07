82d8db068e4d6ee538ff2b01dc9a974a6c0d2c05
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-07 19:05:11 +0200

    links cart and shop vue instances

---
+++ src/scripts/cart.js
@@ -0,0 +1,20 @@
+/*-----------------------------
+    Vue Instance for Cart Page
+----------------------------- */
+
+const cartApp = new Vue({
+
+    // element
+    el : "#cartApp",
+    
+    // members / variables
+    data : {
+        pageTitle : "Cart"
+    },
+
+    // methods / functions
+    methods : {
+
+    }
+
+});
---
+++ src/scripts/shop.js
@@ -0,0 +1,20 @@
+/*-----------------------------
+    Vue Instance for Shop Page
+----------------------------- */
+
+const shopApp = new Vue({
+
+    // element
+    el : "#shopApp",
+    
+    // members / variables
+    data : {
+        pageTitle : "Shop"
+    },
+
+    // methods / functions
+    methods : {
+
+    }
+
+})
