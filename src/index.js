424aa79f5783d6f3ad9a82ab3a909e9fc56eb1f5
NavenRajah <65741660+NavenRajah@users.noreply.github.com>
2022-05-23 22:40:32 +0200

    More products added

--- src/index.js
+++ src/index.js
@@ -39,6 +39,42 @@ data: {
         price:'R700.00',
         imgpath:'image/heroimages/1.png',
       },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png',
+      },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png'
+      },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png',
+      },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png',
+      },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png',
+      },
+      {
+        id:'Jackets',
+        name:'Lacy Jacket',
+        price:'R700.00',
+        imgpath:'image/heroimages/1.png',
+      },
     ],
   },
   })
