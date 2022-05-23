4176feb3b590154f500797bf17ef9af3955b5b5c
NavenRajah <65741660+NavenRajah@users.noreply.github.com>
2022-05-23 23:16:23 +0200

    update to html and Vue

--- src/js/shop.js
+++ src/js/shop.js
@@ -12,10 +12,90 @@ const shopApp = new Vue({
         pageTitle : "Shop",
 
         catalogue : [
-            { id:"#001aa", type:"jacket", name:"Denim Jacket", cost:700.00 },
-            { id:"#001ab", type:"jacket", name:"Leather Jacket", cost:1200.00 },
-            { id:"#002aa", type:"shirt", name:"Denim Shirt", cost:325.00 },
-            { id:"#004ab", type:"pants", name:"Denim Jeans", cost:450.00 },
+            {
+                id:'Jackets',
+                name:'Lad Jacket',
+                description:'Dark and cosy winter warmer',
+                price:'R700.00',
+                imgpath:'image/heroimages/2.png',
+              },
+              {
+                id:'Winter Shrits',
+                name:'Green Winter Polo',
+                description:'Perfect winter woven polo top',
+                price:'R950.00',
+                imgpath:'image/heroimages/1.png'
+              },
+              {
+                id:'Jackets',
+                name:'Leather Jacket',
+                description:'Leather Jacket for all occasions',
+                price:'R7000.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Winter Shirts',
+                name:'White Winter Shirt',
+                description:'Winter shirt with buttons',
+                price:'R620.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Jackets',
+                name:'Green Jacket',
+                description:'Green Jacket with inside pockets',
+                price:'R1700.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Jeans',
+                name:'Denim Jeans',
+                description:'Denim Jeans with rips on knee',
+                price:'R1200.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Jeans',
+                name:'Black Jeans',
+                description:'Mens Black Slim Jeans with pockets',
+                price:'R2700.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Jackets',
+                name:'BLue Jersy',
+                description:'Blue Jearsy with buttons and inside pockets',
+                price:'R300.00',
+                imgpath:'image/heroimages/1.png'
+              },
+              {
+                id:'Shirts',
+                name:'White Formal Shirt',
+                description:'White forml shirt with buttons and silk touch',
+                price:'R800.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Jackets',
+                name:'Brown Winter Jacket',
+                description:'Brown winter Warmer with padding',
+                price:'R700.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Shirts',
+                name:'Round Neck',
+                description:'Round Neck pull over shirt with top pocket',
+                price:'R250.00',
+                imgpath:'image/heroimages/1.png',
+              },
+              {
+                id:'Shirts',
+                name:'Black V Neck',
+                description:'Black v neck tee shirt, short sleeve',
+                price:'R200.00',
+                imgpath:'image/heroimages/1.png',
+              },
         ]
     },
 
